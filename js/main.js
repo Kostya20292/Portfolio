b94038011ec5781d7209727f07dc2c6576d0f69a
Kostya20292 <78802135+Kostya20292@users.noreply.github.com>
2021-12-07 00:39:26 +0300

    Сделан адаптив всего сайта

--- js/main.js
+++ js/main.js
@@ -99,4 +99,13 @@ $(function () {
 
     currentSlider.slick("slickNext");
   });
+
+  const navToggle = $("#navToggle");
+  const nav = $("#nav");
+
+  navToggle.on("click", function (event) {
+    event.preventDefault();
+
+    nav.toggleClass("show");
+  });
 });
